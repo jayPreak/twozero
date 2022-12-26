b8e71d17ecf37303de7b3a5103315159492fdaec
jayPreak <jayesh.preak2003@gmail.com>
2022-12-26 21:13:35 +0530

    ai

--- server/server.js
+++ server/server.js
@@ -4,3 +4,32 @@ import cors from "cors"
 import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+}) 
+
+const openai = new OpenAIApi(configuration)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message:'Hellow from TwoZero'
+    })
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt
+
+        const response = await.openai.createCompletion({
+            
+        })
+    }
+})
+
+
+
